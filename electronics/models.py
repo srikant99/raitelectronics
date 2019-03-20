d4c25f9412ebeb065adf0db78bb9825242b4bf86
srikant <srikantchiluka@gmail.com>
2019-03-20 16:26:43 +0530

    basic page first commit

---
+++ electronics/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+# Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=200)
+    picture = models.ImageField()
+    text = models.TextField()
